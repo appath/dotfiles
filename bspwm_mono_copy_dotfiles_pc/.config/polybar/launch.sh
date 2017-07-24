2fd72f2feb6e81031971ef1b1fae781428e02401
appath <appath@live.ru>
2017-07-24 13:09:55 +0300

    dotfiles

--- bspwm_mono_copy_dotfiles_pc/.config/polybar/launch.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-# terminate already running bar instances
-killall -q polybar
-# wait until the processes have been shut down
-while pgrep -x polybar >/dev/null; do sleep 1; done
-# launch bar
-polybar top &
-
-echo "Polybar launched.."
--- bspwm_mono_copy_dotfiles_pc/.oh-my-zsh/themes/ra.zsh-theme
+++
@@ -1,2 +0,0 @@
-PROMPT="%{$fg[#b79d83]%}>>> "
-RPROMPT="[%{$fg_no_bold[yellow]%}%?%{$reset_color%}]"
