eb4df55b5ddad7846d5b556ec0d9a60d6abd3b4f
Boris241 <htc.boko@gmail.com>
2017-08-10 11:58:24 +0300

    Update config.def.h

--- dwm_google_laptop/dwm_custom/config.def.h
+++ dwm_google_laptop/dwm_custom/config.def.h
@@ -99,7 +99,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-    { MODKEY|ShiftMask,             XK_r,      self_restart,   {0} },
+    	{ MODKEY|ShiftMask,             XK_r,      self_restart,   {0} },
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 };
 
