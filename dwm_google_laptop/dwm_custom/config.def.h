de6b8ad43f4b17af317bc7b4367cb3b249e963bf
Boris241 <htc.boko@gmail.com>
2017-08-10 12:01:23 +0300

    Update config.def.h

--- dwm_google_laptop/dwm_custom/config.def.h
+++ dwm_google_laptop/dwm_custom/config.def.h
@@ -57,7 +57,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "urxvt", NULL };
 
 #include "zoomswap.c"
 static Key keys[] = {
