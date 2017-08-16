7aa5de4873391c43a65ac02d39e7150e85b069b1
Boris241 <htc.boko@gmail.com>
2017-08-16 23:50:18 +0300

    Update gaplessgrid.c

--- dwm_google_laptop/dwm_custom/gaplessgrid.c
+++ dwm_google_laptop/dwm_custom/gaplessgrid.c
@@ -11,14 +11,14 @@ gaplessgrid(Monitor *m) {
 	for(cols = 0; cols <= n/2; cols++)
 		if(cols*cols >= n)
 			break;
-	if(n == 5) /* set layout against the general calculation: not 1:2:2, but 2:3 */
+	if(n == 5)
 		cols = 2;
 	rows = n/cols;
 
 	/* window geometries */
 	cw = cols ? m->ww / cols : m->ww;
-	cn = 0; /* current column number */
-	rn = 0; /* current row number */
+	cn = 0;
+	rn = 0;
 	for(i = 0, c = nexttiled(m->clients); c; i++, c = nexttiled(c->next)) {
 		if(i/rows + 1 > cols - n%cols)
 			rows = n/cols + 1;
