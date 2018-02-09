ebbbd09a63c1fd705bc7a2e1657948a436d24617
Boris241 <htc.boko@gmail.com>
2018-02-09 17:58:38 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,6 +2,7 @@
 
 # Installer ARCH
 # it`s synthetic, makes it easy to install on the hard drive axis.
+# vim :set fileformat=unix save :wq!
 
 cyan='\e[0;36m'
 green='\e[1;32m'
@@ -33,12 +34,12 @@ if test $installer == '1'
 then
     echo
     info="Wait."
-    echo -ne $cyan"Creating a partition and sub-sections"
+    echo -ne "Creating a partition and sub-sections"
     echo ${info}
     mkfs.btrfs -f -L linux /dev/sda
 elif test $installer == '2'
 then
-    echo -ne $cyan"We mount /sdx"
+    echo -ne "We mount /sdx"
     echo
     mount /dev/sda /mnt
 elif test $installer == '3'
@@ -46,14 +47,14 @@ then
     echo -ne $cyan"We create two subtopics"
     btrfs subvolume create /mnt/root
     btrfs subvolume create /mnt/home
-    echo ${info}
-    echo -ne $red"Umount /sdx"
+    echo -ne ${info}
+    echo -ne "Umount /sdx"
     umount /mnt
 elif test $installer == '4'
 then
-    echo -ne $cyan"Compression"
+    echo -ne "Compression"
     mount -o subvol=root,compress=lzo,autodefrag /dev/sda /mnt
-    echo -ne $green"Same"
+    echo -ne "Same"
     mkdir /mnt/home
     mount -o subvol=home,compress=lzo,autoderag /dev/sda /mnt/home
 elif test $installer == '5'
@@ -62,11 +63,11 @@ clear
 exit
 fi
     echo
-    echo -n -e "Back To Menu? [y/n] ";
+    echo -n -e " :: Back To Menu? [yes/no] ";
     read again;
         while [ $again != 'y' ] && [ $again != 'Y' ] && [ $again != 'n' ] && [ $again != 'N' ];
         do
-    echo -n "Back To Menu [y/n] ";
+    echo -n " :: Back To Menu [yes/no] ";
     read again;
     done
 done
